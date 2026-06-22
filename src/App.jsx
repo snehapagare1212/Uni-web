035a8a3311c9bb06a9708123e974b4ed691a21e9
Sneha Pagare <154441020+snehapagare1212@users.noreply.github.com>
2026-06-22 18:08:02 +0530

    all changes

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import Hero from './components/Hero/Hero'
 import Program from './components/Programs/Program'
+import Gallery from './components/gallery/Gallery'
 
 function App() {
   const [count, setCount] = useState(0)
@@ -13,6 +14,7 @@ function App() {
     <div className='container'>
 <Program/>
     </div>
+    <Gallery/>
     
         
     </>
